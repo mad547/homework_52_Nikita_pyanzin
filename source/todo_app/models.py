704564ed9bb1a552c56b3b7fb2ca28740da98394
mad547 <97340914+mad547@users.noreply.github.com>
2026-05-28 14:20:58 +0600

    добавил details в модель

--- source/todo_app/models.py
+++ source/todo_app/models.py
@@ -11,6 +11,7 @@ class Task(models.Model):
     description = models.TextField(null=False, blank=False, verbose_name='Описание')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
     due_date = models.DateField(null=True, blank=True, verbose_name='Дата выпонения')
+    details = models.TextField(null=True, blank=True, verbose_name='Подробное описание')
 
     def __str__(self):
         return self.description[:50]
