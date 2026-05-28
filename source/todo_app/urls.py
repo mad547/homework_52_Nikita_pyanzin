71858b24d388242dba663875a174d14e46634d55
mad547 <97340914+mad547@users.noreply.github.com>
2026-05-28 18:07:25 +0600

    исправил файлы добавил щаблоны и обновил фикстуру

--- source/todo_app/urls.py
+++ source/todo_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from todo_app.views import task_list, task_add, task_delete
+from todo_app.views import task_list, task_add, task_delete, task_detail
 
 
 urlpatterns = [
-    path('tasks/', task_list),
-    path('tasks/add/', task_add),
-    path('tasks/delete/', task_delete),
+    path('tasks/', task_list, name='task_list'),
+    path('tasks/add/', task_add, name='task_add'),
+    path('tasks/delete/', task_delete, name='task_delete'),
+    path('tasks/<int:task_id>/', task_detail, name='task_detail'),
 ]
