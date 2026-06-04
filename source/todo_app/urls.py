9187c3823e6c2e6273e12adf40c3455e87a263e3
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-04 16:41:25 +0600

    обновил urls.py добавил маршруты для удаления и редактирования

--- source/todo_app/urls.py
+++ source/todo_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from todo_app.views import task_list, task_add, task_delete, task_detail
+from todo_app.views import task_list, task_add, task_edit, task_delete, task_detail
 
 
 urlpatterns = [
     path('tasks/', task_list, name='task_list'),
     path('tasks/add/', task_add, name='task_add'),
-    path('tasks/delete/', task_delete, name='task_delete'),
+    path('tasks/<int:task_id>/edit/', task_edit, name='task_edit'),
+    path('tasks/<int:task_id/delete/', task_delete, name='task_delete'),
     path('tasks/<int:task_id>/', task_detail, name='task_detail'),
 ]
