0615bb4d283fc7d8ac16ae596228c8a76e81e9b9
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-04 18:07:22 +0600

    добавил редирект с главной страницы на список задач

--- source/todo_app/urls.py
+++ source/todo_app/urls.py
@@ -3,6 +3,7 @@ from todo_app.views import task_list, task_add, task_edit, task_delete, task_det
 
 
 urlpatterns = [
+    path('', task_list, name='index'),
     path('tasks/', task_list, name='task_list'),
     path('tasks/add/', task_add, name='task_add'),
     path('tasks/<int:task_id>/edit/', task_edit, name='task_edit'),
